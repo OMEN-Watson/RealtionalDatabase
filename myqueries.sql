689de206ee075652de08306b0371e5b167ed4ead
u7568823 <u7568823@anu.edu.au>
2023-08-27 16:26:39 +1000

    dede

--- myqueries.sql
+++ myqueries.sql
@@ -4,10 +4,12 @@ select count(*) from person where year_born <1974 and last_name like '%e';
 
 -- Q2
 
+select round( avg(run_time),2) from movie right join (select* from restriction where country='USA' and description='R' and production_year>1991) as a on a.title=movie.title;
 
 
 -- Q3
 
+ select title ,c from ( select title,count(*) as c from crew group by title) as sub where c=2;
 
 
 -- Q4
